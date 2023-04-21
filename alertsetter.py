bfa9c20089550f56ebfa0d8f5c3c2ef61cbfae1e
her0marodeur <101996103+her0marodeur@users.noreply.github.com>
2023-04-21 23:17:45 +0200

    implemented alertsetter

--- alertsetter.py
+++ alertsetter.py
@@ -1,3 +1,42 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the database connection parameters from environment variables
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+
 def set_alert(term, channel, contact, scoregt=None, scorelt=None, scorechange=None):
-    # Your implementation of send_alert() here.
-    pass
+    try:
+        # Establish a connection to the database
+        conn = psycopg2.connect(
+            dbname=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+
+        # Insert a new row into the "alerts" table
+        cur.execute("INSERT INTO alerts (term, scorelt, contact_method, contact_details) VALUES (%s, %s, %s, %s)",
+                    (term, channel, contact, scoregt, scorelt, scorechange))
+
+        # Commit the transaction to save the changes
+        conn.commit()
+
+        # Close the cursor and database connection
+        cur.close()
+        conn.close()
+    
+    except:
+        return("An error occured")
