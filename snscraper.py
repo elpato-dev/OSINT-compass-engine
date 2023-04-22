a0b9faf83e7823e058017d07b57e2d0e361ec85f
MTAttic <13680882+MTAttic@users.noreply.github.com>
2023-04-22 14:23:46 +0200

    Add reddit user functionality

--- snscraper.py
+++ snscraper.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from snscrape.modules.reddit import RedditSearchScraper
+from snscrape.modules.reddit import RedditSearchScraper, RedditUserScraper
 import snscrape.modules.reddit
 from sentiment_analyzer import get_text_sentiment
 
@@ -10,7 +10,7 @@ def get_snc_instagram_results(user, entries):
 
 def get_snc_reddit_term_results(term, entries, submissions, comments):
     try:
-        scraper = RedditSearchScraper(term, submissions=submissions, comments=comments)
+        scraper = RedditSearchScraper(name=term, submissions=submissions, comments=comments)
         data = []
         results_sentiment = 0
         for i, term in enumerate(scraper.get_items()):
@@ -37,14 +37,58 @@ def get_snc_reddit_term_results(term, entries, submissions, comments):
                 result["link"] = term.link
                 results_sentiment = results_sentiment + get_text_sentiment(str(term.selftext))
             data.append(result)
-            term_data = {
-                "sentiment" : results_sentiment/entries,
-                "search_results" : data}
-            result_data = {
-                "reddit" : term_data
-            }
             if i > entries - 2:
                 break
+        term_data = {
+            "sentiment" : results_sentiment/entries,
+            "results" : data}
+        result_data = {
+            "reddit" : term_data
+            }
+        return result_data
+    except Exception as e:
+        return {"error": str(e)}
+    
+def get_snc_reddit_user_results(user, entries, submissions, comments):
+    try:
+        scraper = RedditUserScraper(name=user, submissions=submissions, comments=comments)
+        data = []
+        results_sentiment = 0
+        sentiment_count = 0
+        for i, term in enumerate(scraper.get_items()):
+            result = {}
+            if type(term) is snscrape.modules.reddit.Comment:
+                result["type"] = "comment"
+                result["id"] = term.id
+                result["author"] = term.author
+                result["created"] = term.date
+                result["subreddit"] = term.subreddit
+                result["url"] = term.url
+                result["parentId"] = term.parentId
+                result["body"] = term.body
+                results_sentiment = results_sentiment + get_text_sentiment(str(term.body))     
+                sentiment_count+=1    
+            elif type(term) is snscrape.modules.reddit.Submission:
+                result["type"] = "submission"
+                result["title"] = term.title
+                result["id"] = term.id
+                result["author"] = term.author
+                result["created"] = term.date
+                result["selftext"] = term.selftext
+                result["subreddit"] = term.subreddit
+                result["url"] = term.url
+                result["link"] = term.link
+                results_sentiment = results_sentiment + get_text_sentiment(str(term.selftext))
+                sentiment_count+=1
+            data.append(result)
+            if i > entries - 2:
+                break
+        term_data = {
+                "sentiment" : results_sentiment/sentiment_count,
+                "results" : data}
+        result_data = {
+                "reddit" : term_data
+            }
         return result_data
     except Exception as e:
         return {"error": str(e)}
