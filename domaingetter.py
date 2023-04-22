2cdf74278edef5cbb14026e6d73de818cacc8a6a
Her0 <her0marodeur@protonmail.com>
2023-04-22 13:00:49 +0200

    added basic domain endpoint

--- domaingetter.py
+++ domaingetter.py
@@ -1,3 +1,31 @@
+import requests
+
 def get_domain_data(domain):
-    # Your implementation of get_domain() here.
-    pass
+
+    # Check robots.txt
+    robots_request = requests.get("https://"+ domain + "/robots.txt")
+    if robots_request.status_code == 200:
+        robots_txt = robots_request.text
+    else:
+        robots_txt = "Not found"
+
+    #check archive.org
+    wayback_request = requests.get("https://archive.org/wayback/available?url="+ domain)
+    
+    wayback_data = wayback_request.json()
+
+    # get subdomains
+
+    subdomain_request = requests.get("https://columbus.elmasy.com/lookup/"+ domain)
+    
+    subdomain_data = subdomain_request.text
+
+
+
+    domain_data = {
+        "robots_txt": robots_txt,
+        "wayback_machine": wayback_data,
+        "subdomains": subdomain_data
+    }
+
+    return domain_data
